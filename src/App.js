0217afdc5b108ce4db4fb0da877a245618e3ddb2
Samir EL KHYATI <samir.elkhyati98@gmail.com>
2022-02-15 20:18:33 +0100

    Reset Password Form - new password

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import SignInForm from "./Components/Form/SignInForm";
+import ResetPasswordForm from "./Components/Form/ResetPasswordForm";
 import "./css/App.css"
 function App() {
   return (
     <div className="App">
-        <SignInForm/>
+        <ResetPasswordForm/>
     </div>
   );
 }
