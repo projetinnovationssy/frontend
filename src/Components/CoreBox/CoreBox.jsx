aa84a565795f6ef2c3079bf7309412ee1c67a986
Samir EL KHYATI <samir.elkhyati98@gmail.com>
2022-02-16 18:27:12 +0100

    creation of uploader pop up

--- src/Components/CoreBox/CoreBox.jsx
+++ src/Components/CoreBox/CoreBox.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styles from "./CoreBox.module.css"
-import EmptyList from "./EmptyList"   
+import EmptyList from "./EmptyList" 
+import Uploader from "./Uploader";  
 class CoreBox extends React.Component{
     constructor(props){
         super(props)
     }
     render(){
         return <div className={styles.coreBox}>
+                <Uploader fileType = "video"/>
                 <EmptyList/>
         </div>
     }
---
+++ src/Components/CoreBox/Uploader.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./CoreBox.module.css"
+import {FaVideo}  from 'react-icons/fa';
+import {GrClose} from "react-icons/gr"
+import Upstyles from "./Uploader.module.css"
+
+class Uploader extends React.Component{
+
+    render(){
+        return <div className={Upstyles.absoluteBox}>
+                <div className={Upstyles.uploader}>
+                    <div className={Upstyles.Uphead}>
+                        <h1 className={Upstyles.h2}>Upload new {this.props.fileType}</h1>
+                        <button className={Upstyles.closebutton} ><GrClose /></button>
+                    </div>
+                    <div className={Upstyles.drag}>
+                        <FaVideo className={Upstyles.icon}/>
+                        <h2 className={Upstyles.h2}>Drag and drop your {this.props.fileType} file to upload</h2>
+                        <p className={Upstyles.p}>Choose a mp3 file from your device</p>
+                        <button className={styles.button}>Select a file</button>
+                    </div>
+                </div>
+
+        </div>
+    }
+}
+
+export default Uploader;
